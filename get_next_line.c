69d1d765062b9c0aaf346aeec2e1eca821a89979
Shenya De Silva <shenya44@outlook.com>
2023-07-21 17:48:59 +0200

    Fixed to pick buffer size from -D flag at compilation

--- get_next_line.c
+++ get_next_line.c
@@ -4,11 +4,8 @@
 #include <errno.h>
 #include <stdlib.h>
 
-#define BUFFER_SIZE 10
-//char	*get_next_line(int fd);
 
-//ssize_t	read(int fildes, void *buf, size_t nbyte);
-//int open (const char* Path, int flags);
+//extern int errno;
 
 extern int errno;
 
@@ -28,6 +25,59 @@ char	*get_next_line(int fd)
 	else
 		return (0);
 }
+
+#ifdef BUFFER_SIZE
+
+ // do this
+/*
+
+char	*get_next_line(int fd)
+{
+	ssize_t ret;
+	void 	*ptr;
+
+	ptr = (void *)malloc(sizeof(char) * 100);
+	if (!ptr)
+		return (NULL);
+	ret = read(fd, ptr, 100);
+	printf("ret :%zd", ret);
+	if (ret != 0) 
+		return((char *)ptr);
+	else
+		return (0);
+}
+*/
+int main(int argc, char **argv)
+{
+	int fd;
+	char *ptr;
+
+	ptr = malloc(sizeof(char) * BUFFER_SIZE);
+	fd = 1;
+	if (argc != 2)
+		return (0);
+	if (argc == 2)
+	{
+		fd = open(argv[1], O_RDONLY);
+		if (fd > 0)
+		{
+			printf("fd: %i \n", fd);
+			ptr = get_next_line(fd);
+			write(1, ptr, 100);
+		}
+		else
+		{
+			printf("Error Number % d\n", errno);
+			perror("Program");
+		}
+	}
+	free (ptr);
+	printf("\n Buffer %d\n", BUFFER_SIZE);
+	return (0);
+}
+
+#else
+#	define BUFFER_SIZE 10
 
 
 int main(int argc, char **argv)
@@ -55,12 +105,8 @@ int main(int argc, char **argv)
 		}
 	}
 	free (ptr);
+	printf("\n Buffer %d\n", BUFFER_SIZE);
 	return (0);
 }
-/*
-    #ifdef BUFFER_SIZE=n   
-       printf("Debug run\n");
-    #else
-       printf("Release run\n");
-    #endif
-	*/
+
+#endif
