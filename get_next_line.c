d0742c8bcd521be855678259d732bfb7af9dbfde
Shenya De Silva <shenya44@outlook.com>
2023-07-22 19:09:28 +0200

    Moved buffer_size definition to header

--- get_next_line.c
+++ get_next_line.c
@@ -1,8 +1,5 @@
 #include "get_next_line.h"
 
-
-#ifdef BUFFER_SIZE
-
 char	*get_next_line(int fd)
 {
 	char 				*ptr;
@@ -27,33 +24,3 @@ if line end is in buffer
 
 	return(ptr);
 }
-
-#else
-# define BUFFER_SIZE 10
-
-char	*get_next_line(int fd)
-{
-	char 				*ptr;
-	ssize_t	read_bytes;
-	//static ssize_t	read_bytes;
-
-	read_bytes = 0;
-	ptr = (char *)malloc(sizeof(char) * BUFFER_SIZE);
-	if (!ptr)
-		return (NULL);
-	read_bytes = read(fd, ptr, BUFFER_SIZE);
-	//read_bytes = read_bytes + read(fd, ptr, BUFFER_SIZE);
-	printf("read_bytes :%zd\n", read_bytes);
-
-/*
-if line end is not in buffer 
-	add buffer to current line
-if line end is in buffer 
-	add upto line end to current line
-*/
-
-
-	return(ptr);
-}
-
-#endif
--- get_next_line.h
+++ get_next_line.h
@@ -5,6 +5,10 @@
 # include <stdio.h>
 # include <stdlib.h>
 
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
 char	*get_next_line(int fd);
 
 #endif
