e3e25dacaf465cd2368a5c519de226c603c3118d
Shenya De Silva <shenya44@outlook.com>
2023-07-28 18:10:17 +0200

    Updated still segfault

--- get_next_line.c
+++ get_next_line.c
@@ -26,10 +26,15 @@ char	*read_from_file(int fd, char *line)
 	char	*temp;
 	int		read_bytes;
 
-	buff = ft_calloc (BUFFER_SIZE + 1, sizeof(char));
+	buff = 0;
+	buff = ft_calloc (BUFFER_SIZE + 2, sizeof(char));
+	if (!buff)
+		return (NULL);
 	read_bytes = read(fd, buff, BUFFER_SIZE);
+	printf("read_bytes %d\n", read_bytes);
 	if (read_bytes <= 0)
 	{
+		//free (buff);
 		return (NULL);
 	}
 	temp = ft_strjoin_fr (line, buff);
@@ -43,6 +48,8 @@ char	*post_process(char *line, char *lb)
 	char	*temp;
 
 	temp = ft_calloc(ft_strlen(line) - ft_strlen(lb + 1) + 1, sizeof(char));
+	if (!temp)
+		return (NULL);
 	ft_strlcpy(temp, line, ft_strlen(line) - ft_strlen(lb + 1));
 	free (line);
 	return (temp);
@@ -52,27 +59,36 @@ char	*get_next_line(int fd)
 {
 	static char *carry_over;
 	char		*line;
+	char		*lb;
 	
+	line = 0;
 	if (!fd || fd < 0)
 		return (NULL);
-	line = 0;
 	if (carry_over != 0)
 	{
 		line = ft_calloc(ft_strlen(carry_over) + 1, sizeof(char));
+		if (!line)
+			return(NULL);
 		ft_strlcpy(line, carry_over, ft_strlen(carry_over) + 1);
+		free (carry_over);
 	}
 	else
+	{
 		line = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+		if (!line)
+			return (NULL);
+	}
 	while (!ft_strchr(line, '\n'))
-			line = read_from_file(fd, line);
+		line = read_from_file(fd, line);
 	if (ft_strchr(line, '\n'))
 	{
-		char	*lb;
-
 		lb = ft_strchr(line, '\n');
 		carry_over = ft_calloc(ft_strlen(lb), sizeof(char));
+		if (!carry_over)
+			return (NULL);
 		ft_strlcpy(carry_over, lb + 1, ft_strlen(lb));
 		line = post_process(line, lb);
+		return (line);
 	}
 	return (line);
 }
