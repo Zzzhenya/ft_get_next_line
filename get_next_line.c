cec58abad1ec9951d802dc2259c870e6aca76368
Shenya De Silva <sde-silv@c4c5c5.42berlin.de>
2023-08-10 18:36:20 +0200

    reverting to old program

--- get_next_line.c
+++ get_next_line.c
@@ -17,6 +17,8 @@ char	*ft_strjoin_fr(char const *s1, char const *s2)
 	size_t	len;
 	char	*ptr;
 
+	if (!s1)
+		s1 = ft_calloc(1, sizeof(char));
 	len = ft_strlen(s1) + ft_strlen(s2);
 	ptr = ft_calloc((len + 1), sizeof(char));
 	if (!ptr)
@@ -42,95 +44,76 @@ char	*read_from_file(int fd, char *line)
 
 	buff = 0;
 	read_bytes = 1;
-	while (read_bytes > 0)
+	while (!ft_strchr(line, '\n') && read_bytes != 0)
 	{
 		buff = ft_calloc (BUFFER_SIZE + 1, sizeof(char));
 		if (!buff)
 			return (NULL);
 		read_bytes = read(fd, buff, BUFFER_SIZE);
-		//printf("%s", buff);
-		if (read_bytes < 0)
+		if (read_bytes <= 0)
 		{
 			free (buff);
-			//printf("%s", line);
 			if (line)
 				free (line);
 			return (NULL);
 		}
+		//if (read_bytes == 0)
+		//	break;
 		line = ft_strjoin_fr (line, buff);
-		if (ft_strchr(line, '\n'))
-			break;
-		//if (read_bytes < BUFFER_SIZE)
-		//	return (line);
-		//printf("%s", line);
+		if (read_bytes < BUFFER_SIZE)
+			return (line);
 	}
-	if (read_bytes == 0)
-	{
-	//	free (buff);
-		printf("%s", line);
-		return (line);
-	}
-	//printf("%s", line);
 	return (line);
+}
+
+char	*post_process(char *line, char *lb)
+{
+	char	*temp;
+
+	temp = ft_calloc(ft_strlen(line) - ft_strlen(lb) + 2, sizeof(char));
+	if (!temp)
+		return (NULL);
+	ft_strlcpy(temp, line, ft_strlen(line) - ft_strlen(lb) + 2);
+	free (line);
+	return (temp);
 }
 
 char	*get_next_line(int fd)
 {
 	static char	*carry_over;
-	char		*line = NULL;
-	char		*lb = NULL;
-	char		*temp;
+	char		*line;
+	char		*lb;
 
-	if (fd < 0 || BUFFER_SIZE <= 0)
+	line = 0;
+	lb = 0;
+	if (fd < 0)
 		return (NULL);
-	if (!ft_strchr(carry_over, '\n'))
-		carry_over = read_from_file(fd, carry_over);
-	if(!carry_over)
-		return (NULL);
-	if (carry_over != 0 && ft_strchr(carry_over, '\n'))
+	if (carry_over != 0)
 	{
-		lb = ft_strchr(carry_over, '\n');
-		if (lb)
+		//if (ft_strlen(carry))
+		line = ft_calloc(ft_strlen(carry_over) + 1, sizeof(char));
+		if (!line)
 		{
-			line = ft_calloc(ft_strlen(carry_over) - ft_strlen(lb) + 2, sizeof(char));
-			if (!line)
-				return (NULL);
-			ft_strlcpy(line, carry_over, ft_strlen(carry_over) - ft_strlen(lb) + 2);
-			temp = ft_calloc(ft_strlen(lb), sizeof(char));
-			if (!temp)
-				return (NULL);
-			ft_strlcpy(temp, lb + 1, ft_strlen(lb));
-			free(carry_over);
-			carry_over = ft_calloc (ft_strlen(temp),sizeof(char));
-			if (!carry_over)
-				return (NULL);
-			ft_strlcpy(carry_over, temp, ft_strlen(temp));
-			free(temp);
+			free (carry_over);
+			return (NULL);
 		}
-	}
-	else if (carry_over != 0 && !ft_strchr(carry_over, '\n'))
-	{
-		line = ft_calloc (ft_strlen(carry_over) ,sizeof(char));
-		if (!line)
-			return (NULL);
-		ft_strlcpy(line, carry_over, ft_strlen(carry_over));
+		ft_strlcpy(line, carry_over, ft_strlen(carry_over) + 1);
 		free (carry_over);
-		return (line);
 	}
-	else if (carry_over == 0)
+	else
 	{
-		line = ft_calloc(1, sizeof(char));
+		line = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
 		if (!line)
 			return (NULL);
-		return (line);
 	}
-	return (line);
-	/*
+	line = read_from_file(fd, line);
 	if (line)
 	{
 		lb = ft_strchr(line, '\n');
 		if (!lb ) //&& ft_strlen(line) == 0)
+		{
 			carry_over = ft_calloc(1, sizeof(char));
+		}
 		else
 		{
 			carry_over = ft_calloc(ft_strlen(lb), sizeof(char));
@@ -141,11 +124,8 @@ char	*get_next_line(int fd)
 			}
 			ft_strlcpy(carry_over, lb + 1, ft_strlen(lb));
 			line = post_process(line, lb);
-
 		}
-	//	return (line);
 	}
-	else
-		return (NULL);
-	*/
+	//	return (line);
+	return (line);
 }
