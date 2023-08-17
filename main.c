53b1e1db8002d283aa048e924fa5eb7bf4e4038d
Shenya De Silva <sde-silv@c4b3c5.42berlin.de>
2023-08-17 14:55:57 +0200

    Removed commented code lines

--- main.c
+++ main.c
@@ -17,55 +17,32 @@ char	*ft_strchr(const char *s, int c);
 
 int main(int argc, char **argv)
 {
-	int fd;
+	int 	fd;
+	char	*ptr;
 
-	//printf("Open_max: %d\n", OPEN_MAX);
-	//printf("Fopen_max: %d\n", FOPEN_MAX);
-	//ptr = malloc(sizeof(char) * BUFFER_SIZE);
 	fd = 1;
 	if (argc < 2)
 	{
 		printf("Where's the text file?\n");
-		return (0);
 	}
 	else if (argc == 2)
 	{
 		fd = open(argv[1], O_RDONLY);
-		if (fd > 0)
+		int i = 1;
+		while (1)
 		{
-			printf("fd: %i\n", fd);
-			
-			int i = 1;
-			char *ptr = "Start";
-			while (ptr)
-			{
-				ptr = get_next_line(fd);
-				printf("%i:%s", i, ptr);
-//				write(1, ptr, strlen(ptr));
-				free(ptr);
-				i ++;
-			}
-			
-			/*
-			printf("\nLine %i : \n", i);
-			//get_next_line(fd);
-			char *ptr = get_next_line(fd);
-			write(1, ptr, strlen(ptr));
-			*/
-			
-		} 
-		//else
-		//{
-		//	printf("Error Number % d\n", errno);
-		//	perror("Program");
-		//}
+			ptr = get_next_line(fd);
+			if (ptr == NULL)
+				break;
+			printf("[%i]:%i:%s", fd, i, ptr);
+			ptr = NULL;
+			i ++;	
+		}
+		close(fd);
 	}
 	else
 	{
 		printf("Too many arguments. Can't argue all day!\n");
-		return (0);
 	}
-
-//	printf("\n Buffer %d\n", BUFFER_SIZE);
 	return (0);
 }
